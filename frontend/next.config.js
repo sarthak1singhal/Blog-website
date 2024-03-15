775f95ef642bda8fb81b096044d0f9599aebe40e
sarthak1singhal <sarthak1singhal@gmail.com>
2024-03-15 16:44:36 +0530

    Create next.config.js
    Added config

---
+++ frontend/next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    publicRuntimeConfig: {
+      APP_NAME: "App Name",
+      API_DEVELOPMENT: "https://api.ventureup.in/api", // Backedn Api Link
+      API_PRODUCTION: "https://api.ventureup.in/api", // enter your production API link
+      PRODUCTION: true,
+      DOMAIN_DEVLOPMENT: "http://localhost:3000",
+      DOMAIN_PRODUCTION: "https://api.ventureup.in/api", // Domain Name For Production
+      FB_APP_ID: " ",
+      DISQUS_SHORTNAME: " Enter Disque shortname ",
+      GOOGLE_CLIENT_ID: "284817623186-nam6ncln33j2e2m9t9j70d84bjuv1936.apps.googleusercontent.com",
+    },
+  };
+  
